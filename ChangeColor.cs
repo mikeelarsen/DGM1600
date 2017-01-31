1a4a43cc4e4d4c4dd3bbb3000a4537d78386ad47
mikeelarsen <mikeelarsen1@gmail.com>
2017-01-31 11:16:42 -0700

    change color
    change color function

---
+++ ChangeColor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) { 
+			GetComponent<Renderer> ().material.color = Color.red; 
+		} 
+		if (Input.GetKeyDown (KeyCode.G)) { 
+			GetComponenet<Renderer> ().material.color = Color.green; 
+		}
+
+		if (Input.GetKeyDown (KeyCode.B)) { 
+			GetComponenet<Renderer> ().material.color = Color.blue; 
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) { 
+			GetComponenet<Renderer> ().material.color = Color.yellow; 
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) { 
+			GetComponenet<Renderer> ().material.color = Color.black; 
+		}
+
+		if (Input.GetKeyDown (KeyCode.Delete)) { 
+			GetComponenet<Renderer> ().material.color = Color.white; 
+		}
+	
+	}
+}
