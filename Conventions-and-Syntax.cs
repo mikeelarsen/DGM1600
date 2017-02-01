26a3684829a5914d79af198c164edb885720546d
mikeelarsen <mikeelarsen1@gmail.com>
2017-02-01 01:02:45 -0700

    Add files via upload

---
+++ Conventions-and-Syntax.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+    //Classes inherit sets of traits which are defined here:
+    //for examples: if we use the class "Mammal", most mammals have:
+    //Fur/hair
+    //2 eyes
+    //four legs/limbs
+    //a tail
+    //a backbone
+    //etc.
+
+        //then we make something which inherits from the parent class,
+        //an example of this would be cat, which is a sub-class of mammal.
+        //cats also have whiskers
+        //very flexible and dextrous
+        //etc.
+
+    //good syntax includes:
+        //proper indentation
+            //when something is indented within code it is easier to follow
+        //using semicolons to end statements
+            //this ensures the compiler knows how to parse your code.
+        //writing in CamelCase
+            //by capitalizingTheFirstLetter in oneLongString, you can easily
+            //read variables, while still conforming with allowed syntax.
+        //COMMENTING in your code
+            //this is useful, not only for those who will be reading it, but
+            //useful too, for you the developer to remind yourself of certain
+            //stages within the functions and lines of code.
+        //utilizing whitespace
+            //along with indenting, adding whitespace helps the code to
+            //"breath" and assists with comprehension and legibility.
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
